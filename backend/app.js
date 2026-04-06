b0354be5110b978d6ea42c38cb8bbba12a283a1a
ZakariaAkrabou <zakariajr963@gmail.com>
2026-04-06 22:04:29 +0100

    adding contact methode for cutomer

--- backend/app.js
+++ backend/app.js
@@ -1,13 +1,16 @@
 import express from "express";
 import dotenv from "dotenv";
 import connectDB from "./config/database.js";
+
 import authRoutes from "./routes/auth.route.js";
 import serviceRoutes from "./routes/service.route.js";
 import userRoutes from "./routes/user.route.js";
 import adminRoutes from "./routes/admin.route.js";
-import cors from "cors";
 import reviewRoutes from "./routes/review.route.js";
+import contactRoutes from "./routes/contact.route.js";
 import customerRoutes from "./routes/customer.route.js";
+
+import cors from "cors";
 import { initSocket } from "./config/socket.js";
 import http from "http";
 
@@ -24,6 +27,8 @@ app.use("/api/services", serviceRoutes);
 app.use("/api/user", userRoutes);
 app.use("/api/admin", adminRoutes);
 app.use("/api/customer", customerRoutes);
+app.use("/api/reviews", reviewRoutes);
+
 
 
 const PORT = process.env.PORT || 5000;

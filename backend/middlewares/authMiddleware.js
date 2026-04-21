67f1d6360d9f9c285d3afd50c8a20ae021ca893c
ZakariaAkrabou <zakariajr963@gmail.com>
2026-04-21 13:19:43 +0100

    fixing auth middleware to send token into http cookies

--- backend/middlewares/authMiddleware.js
+++ backend/middlewares/authMiddleware.js
@@ -1,15 +1,18 @@
 import jwt from "jsonwebtoken";
 
 export const authenticated = (req, res, next) => {
-  const token = req.header("Authorization")?.replace("Bearer ", "");
+ 
+  let token = req.header("Authorization")?.replace("Bearer ", "");
+  if (!token && req.cookies && req.cookies.token) {
+    token = req.cookies.token;
+  }
   if (!token) {
     return res.status(401).json({ message: "Unauthorized" });
   }
 
   try {
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
-
-        req.user = decoded;
+    req.user = decoded;
     next();
   } catch (err) {
     return res.status(401).json({ message: "Token is not valid" });

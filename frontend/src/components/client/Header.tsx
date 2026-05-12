070c46a2a2488a8ddda00890c6199557090362c2
fati-aafak <fatimaahamam123@gmail.com>
2026-05-12 15:41:59 +0100

    add logo

--- frontend/src/components/client/Header.tsx
+++ frontend/src/components/client/Header.tsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import logo from "../../assets/logoservicehub.png"; // Garde ton import
+import logo from "../../assets/logo2.png"; 
 
 const Header: React.FC = () => {
   const [open, setOpen] = useState(false);
